516acde140ace6f772567ad9d10f416b338bc4e4
izul-wq <ezi4123@gmail.com>
2024-09-25 15:10:00 +0700

    240441100060_AhmadAzizulArif_KakKamal

---
+++ modul 7/24_060_AhmadAzizulArif/modul 1.py
@@ -0,0 +1,37 @@
+# a = "dani"
+# nama = "danu"
+# _kelas = "apel"
+# print(a)
+# print(nama)
+# print(nama, _kelas)
+
+# aritmatika
+a = 20
+b = 10
+# penjumlahan cara 1
+print("Hasil penjumlahan a ditambah b adalah : ", a + b)
+
+
+# pengurangan cara 2
+z = a - b
+print("hasil pengurangan dari a dikurangi b adalah :", z)
+
+# perkalian
+# nilai
+e = 25
+f = 5
+# rumus
+g = e * f
+print("hasil perkalian dari e dikali f adalah : ", g)
+
+# pembagian ada 3
+# 1 pembagian 
+print("hasil pembagian dari a dibagi b adalah : ", a / b)
+
+# 2 pembagian bilangan bulat kebawah
+print("hasil pembagian dari a dibagi b adalah : ", a // b)
+
+# 3 pembagian modulus
+print("hasil pembagian dari a dibagi b adalah : ", a % b)
+
+print(10**3)
