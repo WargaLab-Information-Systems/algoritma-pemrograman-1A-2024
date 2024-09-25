5c2e41b19e3fcdb0e59ddab3010410018ee81f4f
liatulmukarromah <liatulmukarromah@gmail.com>
2024-09-25 15:09:47 +0700

    240441100116_Viscaabelia

---
+++ modul 7/240441100116_ViscaAbelia/praktikum1.py
@@ -0,0 +1,15 @@
+# a="visca"
+# Nama="danu"
+# _kelas=10
+# print(Nama, _kelas)
+
+#aritmatika
+a=20
+b=10
+#penjumlahan
+# c=a+b
+# print("hasil penjumlahan a ditambah b adalah:", a + b)
+
+#pengurangan
+c=a-b
+print("hasil pengurangan a dikurangi b adalah :", c)
