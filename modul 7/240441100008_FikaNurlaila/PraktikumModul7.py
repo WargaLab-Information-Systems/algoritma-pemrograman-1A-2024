46f4daca7ae95912c592c04b07c76687b6360e04
Fika_cute <ankhachiil@gmail.com>
2024-09-25 15:10:44 +0700

    240441100008_FikaNurlaila_kakMuhammadPutraSenjaKaimana

---
+++ modul 7/240441100008_FikaNurlaila/PraktikumModul7.py
@@ -0,0 +1,35 @@
+
+nama= "dani"
+a="danu"
+_kelas=10
+print(nama, _kelas)
+
+#Aritmatika
+a=10
+b=20
+
+#penjumlahan
+c=a+b
+print ("hasil penjumlahan a di tambah b adalah ", c)
+
+#pengurangan
+c=a-b
+print("hasil pengurangan a dikurang b adalah", c)
+
+#perkalian
+a=12
+b=45
+print("hasil perkalian dari a dikali b adalah", a*b)
+
+#pembagian
+b=90
+f=5
+print("hasil dari pembagian b/f adalah", b/f)
+
+#pembagian bulat
+print(2//7)
+#modulus
+print(2%7)
+
+#perpangkatan
+print(10**2)
