878eb496b22fd52a3948f2e40c1f230074a8f3da
AHMAD WARISI <ahmdwarisi2@gmail.com>
2024-10-01 20:38:59 +0700

    240441100108-AHMAD WARISI-MOHAMMAD RIFQI RAZZAN PRATAMA-MODUL1 (#62)
    * 240441100108_AhmadWarisi_KakRazzan
    
    * MODUL1_240441100108_AHMADWARISI_MOHAMMADRIFQIRAZZANPRATAMA

---
+++ modul 7/240441100108_AhmadWarisi/Modul-1.py
@@ -0,0 +1,27 @@
+# nama = "dani"
+# Nama = "danu"
+# print (nama)
+
+# _kelas=10
+# print(nama, _kelas)
+
+# # aritmatika
+# a=20
+# b=10
+# #penjumlahan
+# c=a+b
+# print("hasil penumlahan dari a ditambah b adalah :",a+b)
+
+#pengurangan
+# c=a-b
+# print ("hasil pengurangan dari a dikurangi b adalah :",c)
+
+#perkalian
+d=12
+e=11
+# print ("hasil perkalian :", d*e)
+
+#pembagian
+print("hasil dari baginya adalah :", d/e)
+
+
