f29e8b9158ca45cec80d297b3c95d225d6564048
yesicapitriariani <yesicapitriariani5@gmail.com>
2024-10-01 13:36:03 +0700

    240441100096-YESICA PITRI ARIANI-MOHAMMAD RIFQI RAZZAN PRATAMA-MODUL 1 (#51)
    * 240441100096_YesicaPitriAriani_KakRazzan
    
    * 240441100096
    
    * 240441100096-YESICA PITRI ARIANI-MOHAMMAD RIFQI RAZZAN PRATAMA-MODUL 1
    
    ---------
    
    Co-authored-by: yesicapitriani <yesicapitriani@gmail.com>

---
+++ modul 1/240441100096_YesicaPitriAriani/YESICA/nomor 4.py
@@ -0,0 +1,15 @@
+# tugas nomer 4
+#jumlah orang yang ada
+n = 7
+
+#jumlah orang yang dipilih ke kelompok
+r = 4
+
+faktorial_7=7*6*5*4*3*2*1
+faktorial_4=4*3*2*1
+faktorial_3=3*2*1
+
+hasil_faktorial = faktorial_7 / (faktorial_4*faktorial_3)
+
+print("jadi hasil", hasil_faktorial, "cara untuk memilih 4 orang dari 7 orang")
+
