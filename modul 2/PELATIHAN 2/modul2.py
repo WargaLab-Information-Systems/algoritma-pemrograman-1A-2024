a2f7b28e849ecdac69ddfc309c25a0d27ef1c477
yesicapitriani <yesicapitriani@gmail.com>
2024-10-08 18:30:41 +0700

    240441100096_YesicaPitriAriani_MODUL2_KakRazzan

---
+++ modul 2/PELATIHAN 2/modul2.py
@@ -0,0 +1,65 @@
+# #Perintah if
+# kota = "nganjuk"
+# asal =input("Masukkan asal kamu =")
+# if asal == kota:
+#     print("asal saya dari", asal)
+
+# # perintah if-else
+# angka = int(input("masukkan angka lebih dari 5 ="))
+# if angka <= 5:
+#     print("angka kurang dari 5")
+# else:
+#     print("angka adalah 5 atau lebih")
+
+# # perintah if-elif-else
+# umur = int(input("masukkan umur anda"))
+# if umur <= 21:
+#     print("anda belum dewasa")
+# elif umur < 100:
+#     print("anda sudah dewasa")
+# elif umur == 100:
+#     print("anda tua banget")
+# else:
+#     print("anda alien")
+
+# perintah if bersarang 
+# umur = int(input("masukkan umur anda"))
+# if umur <= 21:
+#     print("anda remaja")
+#     if umur <=45:
+#        print("anda dewasa")
+#     elif umur <=49:
+#        print("anda hampir paruhbaya")
+#     else:
+#        print("anda sudah paruhbaya")
+
+# elif umur <=100:
+#      print("anda alien")
+# else:
+#     print("anda alien")
+
+
+# #TUGAS DARI KAK RAZZAN untuk mencari genap/ganjil
+# angka = int (input("masukkan angka"))
+# if angka % 2 == 0:
+#     print (f"bilangan {angka} adalahbilangan genap")
+# else :
+#     print (f"bilangan {angka} adalah bilangan ganjil")
+
+#ternary if else 
+# waktu = 20
+# print("selamat malam") if waktu >18 else print("selamat pagi/sore")
+
+A = "A"
+b = 10
+c = 20
+if (A == 10 and B == 10 or C == 20):
+    print("ini yang keluar A")
+
+elif (A == "A" or B == 20 and C == 20):
+    print("ini yang keluar B")
+  
+ 
+
+
+
