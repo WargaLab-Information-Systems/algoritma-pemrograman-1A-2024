2d0c967b07e427d59f75a3be013c57b2db25426d
Davingit12 <davinjb2005@gmail.com>
2024-10-15 13:16:09 +0700

    '1'

---
+++ modul 2/latihan praktikum.py/modul3.py
@@ -0,0 +1,19 @@
+# i = 10
+# for i in range(0,i,1):
+#     if  i == 2:
+#         print(i)
+#         pass
+#     print(i)
+#     print (2);
+# print (3);
+
+t = 10
+for i in range (1,t,2):
+    for j in range(i):
+        print(j * "*")
+    print(i * " ")    
+
+    # for i in range(t):
+    #     akan di print
+    #     akan di print
+    # tidak akan di print
