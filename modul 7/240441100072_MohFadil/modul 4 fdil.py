a779450e4fdf369710f56bcc1deeb013886c0ac3
Fadil-ops <muhamadfadildil@gmail.com>
2024-11-02 21:51:49 +0700

    240441100072_MOH FADIL_ANDIKA KAMALUDDIN_MODUL 4 (#135)
    * 240441100072-MOH FADIL-ANDIKA KAMALUDDIN-MODUL 1
    
    * 240441100072_MOHFADIL_ANDIKAKAMALUDDIN_MODUL2
    
    * 240441100072-MOH FADIL-ANDIKA KAMALUDDIN-MODUL 3
    
    * 240441100072_MOH FADIL_ANDIKA KAMALUDDIN_MODUL 4

---
+++ modul 7/240441100072_MohFadil/modul 4 fdil.py
@@ -0,0 +1,28 @@
+#nasted loops
+# #n = 11
+# nilai = 1
+# for i in range(2): #kolom
+#     for j in range(3): #baris
+#         print(nilai, end =" ")
+#         nilai += 1
+#     print()
+
+
+
+# a = int(input("masukan bilangan pertama:"))
+# b = int(input("masukan bilangan kedua:"))
+
+# if a <=0 or b <=0:
+#     print("masukan bilangan positif")
+# else:
+#     while b!=0:
+#         a,b = b, a%b
+#     print(f"FPB nya adalah : {a}")
+
+#pyramid
+# n=5
+# for i in range(1, n + 1):
+#     print(" " * (n - 1), end ="")
+#     for j in range(1, i + 1):
+#         print(str(j), end=" ")
+#     print()
