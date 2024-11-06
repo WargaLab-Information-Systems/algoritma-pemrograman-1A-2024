cc7dea7d3ab88902f901428aa59771a81c189782
yesicapitriariani <yesicapitriariani5@gmail.com>
2024-11-06 18:36:52 +0700

    240441100096-YESICA PITRI ARIANI-MOHAMMAD RIFQI RAZZAN PRATAMA-MODUL 5 (#169)
    * 240441100096_YesicaPitriAriani_KakRazzan
    
    * 240441100096
    
    * 240441100096-YESICA PITRI ARIANI-MOHAMMAD RIFQI RAZZAN PRATAMA-MODUL 1
    
    * 240441100096_YesicaPitriAriani_MODUL2_KakRazzan
    
    * 240441100096-YesicaPitriAriani-MohammadRifqiRazzanPratama-MODUL3
    
    * 240441100096-YESICA PITRI ARIANI-MOHAMMAD RIFQI RAZZAN PRATAMA-MODUL 4
    
    * 240441100096-YESICA PITRI ARIANI-MOHAMMAD RIFQI RAZZAN PRATAMA-MODUL5
    
    ---------
    
    Co-authored-by: yesicapitriani <yesicapitriani@gmail.com>

---
+++ modul 5/240441100096-YesicaPitriAriani/modul5.py.py
@@ -0,0 +1,86 @@
+# nomor 1
+# def calculate_discount(total_belanja, jenis_keanggotaan): 
+#     diskon = 0 
+#     if total_belanja > 1000000: 
+#         diskon += 5 
+#     if jenis_keanggotaan.lower() == "gold": 
+#         diskon += 15 
+#     elif jenis_keanggotaan.lower() == "silver": 
+#         diskon += 10 
+#     elif jenis_keanggotaan.lower() == "bronze": 
+#         diskon += 5 
+ 
+#     jumlah_diskon = total_belanja *(diskon/100) 
+#     total_setelah_diskon = total_belanja - jumlah_diskon 
+ 
+#     return jumlah_diskon, total_setelah_diskon 
+ 
+# jenis_keanggotaan = str(input("punya kartu anggota? (punya/tidak)")) 
+# if jenis_keanggotaan == "punya": 
+#     jenis_keanggotaan = input("Masukkan jenis keanggotaan (Gold/Silver/Bronze): ") 
+#     total_belanja = int(input("Masukkan total belanja: ")) 
+#     calculate_discount(total_belanja, jenis_keanggotaan) 
+ 
+# elif jenis_keanggotaan == "tidak": 
+#     jenis_keanggotaan == "tidak" 
+#     total_belanja = int(input("Masukkan total belanja: ")) 
+#     calculate_discount(total_belanja, jenis_keanggotaan) 
+ 
+# jumlah_diskon, total_setelah_diskon = calculate_discount(total_belanja, 
+# jenis_keanggotaan)  
+ 
+# print(f"Jumlah diskon: Rp {jumlah_diskon:}") 
+# print(f"Total yang harus dibayar: Rp {total_setelah_diskon:}") 
+
+
+
+# nomor 2
+# def fibonacci(n): 
+#     if n == 0: 
+#         return 0 
+#     elif n == 1: 
+#         return 1 
+#     else: 
+#         return fibonacci (n-1) + fibonacci(n-2) 
+# n = int(input("masukkan angka yang ingin di hitung : ")) 
+# print(f"hasilnya adalah {fibonacci(n)}") 
+
+
+
+# nomor 3
+# def faktorial(angka): 
+#     "fungsi faktorial" 
+#     faktorial = 1 
+#     for i in range (1, angka + 1): 
+#         faktorial *= i 
+#     return faktorial 
+ 
+# angka = int(input("masukkan angka: ")) 
+ 
+# print(f"{angka}! =", end=" ") 
+# for i in range (angka, 0, -1): 
+#     print(i, end= " x " if i > 1 else " = ") 
+# print(faktorial(angka))
+
+
+
+
+# # # nomor 4
+# def palindrom(kata): 
+#     kata = kata.replace(" ", "")   
+#     return kata == kata[::-1]  
+ 
+# while True: 
+#     input_kata = input("Masukkan kata untuk memeriksa apakah itu  palindrom: ") 
+
+#     if input_kata.isdigit(): 
+#         print("Input tidak valid. Silakan masukkan kata, bukan angka.") 
+#     elif isinstance(input_kata, str): 
+
+#         if palindrom(input_kata): 
+#             print(f"'{input_kata}' adalah palindrom.") 
+#         else: 
+#             print(f"'{input_kata}' bukan palindrom.")
+#         break 
+#     else: 
+#         print("Input tidak valid. Silakan masukkan kata.") 
