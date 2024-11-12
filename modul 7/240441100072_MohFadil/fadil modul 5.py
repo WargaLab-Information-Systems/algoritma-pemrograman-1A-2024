b3552f3cfcc7966b4cb54260132674ecf30e86fd
Fadil-ops <muhamadfadildil@gmail.com>
2024-11-12 11:03:33 +0700

    240441100072_MOH FADIL_ANDIKA KAMALUDDIN_MODUL 5 (#185)
    * 240441100072-MOH FADIL-ANDIKA KAMALUDDIN-MODUL 1
    
    * 240441100072_MOHFADIL_ANDIKAKAMALUDDIN_MODUL2
    
    * 240441100072-MOH FADIL-ANDIKA KAMALUDDIN-MODUL 3
    
    * 240441100072_MOH FADIL_ANDIKA KAMALUDDIN_MODUL 4
    
    * 240441100072_MOH FADIL_ANDIKA KAMALUDDIN_MODUL 5
    
    ---------
    
    Co-authored-by: Andika Kamaluddin <143859991+yukiinon@users.noreply.github.com>

---
+++ modul 7/240441100072_MohFadil/fadil modul 5.py
@@ -0,0 +1,40 @@
+# a = 1
+# b = 2
+
+
+# def tambah (a,b):
+#     print(a+b)
+
+# tambah(a,b)
+
+
+# d = 1
+# e = 2
+# def tambah (a,b):
+#     c = a + b
+#     return c
+
+# print(tambah(d,e))
+
+
+# n= 4
+# m= 4
+# x= lambda a,b : a*b
+# print(x(n,m))
+
+
+
+
+
+#challange
+
+# a=5
+# b=5
+# def kalkulator(a,b):
+#     while true:
+#       print("1.penjumlahan")
+#       print("2.pengurangan")
+#       print("3.perkalian")
+#       print("4.pembagian")
+#       print("5.keluar")
+#       c = int(input("pilih menu"))
